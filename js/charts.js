a10635590eb10a887ca3ae6610599af1efc85a7b
Godfrey Takavarasha <godfrey.takavarasha@gmail.com>
2014-09-19 10:51:24 -0400

    removesenegal

--- js/charts.js
+++ js/charts.js
@@ -1,69 +1,3 @@
-//var casesColorPalette = ['rgb(255,237,160)','rgb(254,178,76)','rgb(240,59,32)'];
-//var deathsColorPalette = ['rgb(236,226,240)','rgb(166,189,219)','rgb(28,144,153)'];
-var casesColorPalette = ['rgb(255,237,160)','rgb(95, 107, 119)','rgb(240,59,32)'];
-var deathsColorPalette = ['rgb(0,0,0)','rgb(166,189,219)','rgb(28,144,153)'];
-var cases
-
-/*
-This function generates the stacked bar graph
-*/
-function generateEbolaStackedBarChart(bindTo, url) {
-	return c3.generate({
-		bindto: bindTo,
-		data: {
-			url: url,
-			type: 'bar',
-			groups: [
-				['confirmed_cases', 'probable_cases', 'suspected_cases'],
-				['confirmed_deaths', 'probable_deaths', 'suspected_deaths']
-			],
-			x: "country",
-			names: {
-				confirmed_cases: "Confirmed Cases",
-				probable_cases: "Probable Cases",
-				suspected_cases: "Suspected Cases",
-				confirmed_deaths: "Confirmed Deaths",
-				probable_deaths: "Probable Deaths",
-				suspected_deaths: "Suspected Deaths"			
-			},
-			colors: {
-				confirmed_cases: casesColorPalette[2],
-				probable_cases: casesColorPalette[1],
-				suspected_cases: casesColorPalette[0],
-				confirmed_deaths: deathsColorPalette[2],
-				probable_deaths: deathsColorPalette[1],
-				suspected_deaths: deathsColorPalette[0]			
-			}
-		},
-		axis: {
-			y: {
-				show: true,
-				max: 2000
-			},
-			x: {
-				show: true,
-				label: {
-					position: "middle"
-				},
-				type: "category"
-			}		
-		},
-		tooltip: {
-			format: {
-				value: d3.format(',')
-			}
-		},
-		size: {
-			height: 300
-		},
-		bar: {
-			width: {
-				ratio: 0.5 // this makes bar width 50% of length between ticks
-			}
-		}
-	});
-};
-
 function getMainChartData(data) {
 	return [
 		['country', '2014-08-29', '2014-09-05', '2014-09-08', '2014-09-12', '2014-09-16', '2014-09-18'], 
@@ -129,10 +63,37 @@ function generateMainChart(bindTo,data) {
 			types: {
 				Guinea: "spline",
 				Liberia: "spline",
-				Nigeria: "Spline",
-				SierraLeone: "area-spline",
+				Nigeria: "spline",
+				SierraLeone: "spline",
 				Senegal: "spline"
 			}
+		},
+		axis: {
+			x: {
+				type: 'timeseries',
+				tick: {format: '%Y-%m-%d'}
+			}
+		},
+		size: {
+			height: 300
+		}
+	});
+};
+
+function generateMainChartB(bindTo,data) {
+	return c3.generate({
+		bindto: bindTo,
+		data: {
+			x: "country",
+			columns: getMainChartData(),
+			types: {
+				Guinea: "area-spline",
+				Liberia: "area-spline",
+				Nigeria: "area-spline",
+				SierraLeone: "area-spline",
+				Senegal: "area-spline"
+			},
+			groups: [["Guinea", "Liberia", "Nigeria", "SierraLeone", "Senegal"]]
 		},
 		axis: {
 			x: {
@@ -164,36 +125,11 @@ function generateCountryChart(bindTo, data, country) {
 			x: {
 				type: 'timeseries',
 				tick: {format: '%Y-%m-%d'}
-			}
+			},
+			y: {max: 3000}
 		},
 		size: {
 			height: 300
-		}
-	});
-}
-/*
-This function generate a gauge chart for a country 
-*/
-function generateGaugeChart(bindTo, value) {
-	return c3.generate({
-		bindto: bindTo,
-		data: {
-			columns: [
-				["data", value],
-			],
-			type: 'gauge'
-		},
-		color: {
-			pattern: ["#FF0000", "#F97600", "F6C600", "#60B044"]
-		},
-		tooltip: {
-			format: {
-				value: d3.format(',')
-			}
-		},
-		size: {
-			height: 100,
-			width: 100
 		}
 	});
 };
